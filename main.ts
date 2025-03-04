a2d13293bc4bac77554c79c88d90338d745534c1
Malav S. <thisismalav@gmail.com>
2025-03-03 18:27:38 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-basic.forever(function () {
-    if (input.magneticForce(Dimension.Strength) < 0 && input.magneticForce(Dimension.Strength) > 0) {
-        control.waitMicros(1000000)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.LoopingInBackground)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-    } else {
-        music.stopAllSounds()
-        pins.digitalWritePin(DigitalPin.P0, 0)
-    }
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
 })
